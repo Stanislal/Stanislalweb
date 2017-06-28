8446b62918472eefd02ba140d45271d467cd761c
Icaro Baruffi <icru3@hotmail.com>
2017-06-28 00:58:58 -0300

    Layout base do projeto

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,27 @@
+<div class="size-header-top-left">
+<p>Visite a seção biblioteca para mais informações sobre o <b>Stanislal Project</b></p>
+</div>
+
+<div class="size-header-top-right">
+Bem vindo ao <b>Stanislal Project</b>
+<button type="button" class="btn btn-success"> Saber mais</button>
+
+</div>
+
+<style media="screen">
+  .size-header-top-left {
+    width: 50%;
+    padding-top:8px;
+  }
+  .size-header-top-right {
+    width: 50%;
+    text-align: right;
+  }
+
+  .size-header-top-right .btn-success {
+    margin-left: 20px;
+    color: #dad6bf;
+    background-color: #4caf50;
+    text-shadow: 0px 1px 5px #0c0c0c;
+  }
+</style>
